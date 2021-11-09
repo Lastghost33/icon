0164b7de134fc425674bbdd2fad562d4775d560a
lastghost33 <tnhn33dyg@gmail.com>
2021-11-09 17:16:46 +0300

    1111

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:icon/icon__lastghost_icons.dart';
+
+void main() {
+  runApp(const Exapp());
+}
+
+class Exapp extends StatelessWidget {
+  const Exapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Icon(
+              Icon_Lastghost.coffee,
+              size: 40,
+            ),
+          ),
+          Expanded(
+            child: Icon(
+              Icon_Lastghost.bed,
+              size: 40,
+            ),
+          ),
+          Expanded(
+            child: Icon(
+              Icon_Lastghost.bed,
+              size: 40,
+            ),
+          ),
+          Expanded(
+            child: Icon(
+              Icon_Lastghost.hamburger,
+              size: 40,
+            ),
+          ),
+          Expanded(
+            child: Icon(
+              Icon_Lastghost.profile,
+              size: 40,
+            ),
+          ),
+        ],
+      )),
+    );
+  }
+}
